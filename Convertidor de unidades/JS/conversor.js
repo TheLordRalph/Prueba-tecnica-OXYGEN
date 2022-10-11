fd8fa1db7d6e74333a98208b5923a22b94aced0f
TheLordRalph <raulmoretamartin@gmail.com>
2022-10-11 18:35:20 +0200

    Añadido el guardado en localStorage.

--- Convertidor de unidades/JS/conversor.js
+++ Convertidor de unidades/JS/conversor.js
@@ -1,10 +1,27 @@
+var saveds = 0;
+
+
+window.onload = function () {
+
+    let saved = document.getElementById("saved");
+    if (localStorage.length >= 1) {
+        let i = 0;
+        while (i <= localStorage.length) {
+            if (localStorage.getItem(i) != null) {
+                saved.innerHTML += localStorage.getItem(i);
+            }
+            i++;
+        } 
+    }
+
+
+}
+
 const Valores = {
     miles: 0.6213712,
     feet: 3.28084,
     inch: 0.3937008
 }
-
-var saveds = 0;
 
 function changeUnit() {
     let selectUnidad = document.getElementById("selectUnidad");
@@ -70,8 +87,17 @@ function conversion() {
 function saved() {
     const div = document.createElement("div");
     div.setAttribute("class", "favorite");
-    div.setAttribute("id", saveds);
-    saveds += 1;
+
+    while (true) {
+        if (localStorage.getItem(saveds) == null) {
+            div.setAttribute("id", saveds);
+            break;
+        } else {
+            saveds++;
+        }
+    }
+    
+    
     div.innerHTML = "<h1></h1><svg onclick=\"deleteSaved(this)\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" fill=\"currentColor\" class=\"bi bi-x-lg\" viewBox=\"0 0 16 16\"><path d=\"M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z\"/></svg>";
     
     let numeroIntroducido = document.getElementById("numeroIntroducido");
@@ -92,8 +118,13 @@ function saved() {
     
     numeroIntroducido.value = "";
     conversion();
+
+    localStorage.setItem(saveds, div.outerHTML);
+    saveds += 1;
 }
 
 function deleteSaved(element) {
     document.getElementById("saved").removeChild(element.parentNode);
+
+    localStorage.removeItem(element.parentNode.id);
 }

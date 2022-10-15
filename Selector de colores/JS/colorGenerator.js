793c8ae856edcceb398b96fd11ea93b6e4bafaa0
TheLordRalph <raulmoretamartin@gmail.com>
2022-10-15 20:10:59 +0200

    Fncionamiento para guardar la paleta de colores, y guardandose en el 'LocalStorage'

--- Selector de colores/JS/colorGenerator.js
+++ Selector de colores/JS/colorGenerator.js
@@ -1,9 +1,21 @@
 var colorPorDefecto;
 var colorPalette = "color1";
+var saveds = 0;
 
 window.onload = function() {
     colorPorDefecto = document.getElementById("colorActive");
     selectColor();
+
+    let saved = document.getElementById("saved");
+    if (localStorage.length >= 1) {
+        let i = 0;
+        while (i <= localStorage.length) {
+            if (localStorage.getItem(i) != null) {
+                saved.innerHTML += localStorage.getItem(i);
+            }
+            i++;
+        } 
+    }
 }
 
 function changPallete(element) {
@@ -169,4 +181,56 @@ function selectColorDefault(element) {
     colorPorDefecto.id = "";
     element.id = "colorActive";
     colorPorDefecto = element;
+}
+
+function createSaved() {
+    let inputName = document.getElementById("inputName");
+    let color1 = document.getElementById("color1");
+    let color2 = document.getElementById("color2");
+    let color3 = document.getElementById("color3");
+    let color4 = document.getElementById("color4");
+    let color5 = document.getElementById("color5");
+
+
+    if (inputName.value != "" && color1.title != "" && color2.title != "" && color3.title != "" && color4.title != "" && color5.title != "") {
+        let saved = document.getElementById("saved");
+        const div = document.createElement("div");
+
+        while (true) {
+            if (localStorage.getItem(saveds) == null) {
+                div.setAttribute("id", saveds);
+                break;
+            } else {
+                saveds++;
+            }
+        }
+
+        div.innerHTML = "<div id=\"saved " + saveds +"\" class=\"favorite\"><h5\>" + inputName.value +"</h5><svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\"><path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/><path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/></svg><div class=\"colorsSaved\"><div id=\"saved " + saveds + "-1\" style=\"background-color: " + color1.title +"\"></div><div id=\"saved " + saveds + "-2\" style=\"background-color: " + color2.title +"\"></div><div id=\"saved " + saveds + "-3\" style=\"background-color: " + color3.title +"\"></div><div id=\"saved " + saveds + "-4\" style=\"background-color: " + color4.title +"\"></div><div id=\"saved " + saveds + "-5\" style=\"background-color: " + color5.title +"\"></div></div></div>";
+    
+        saved.appendChild(div);
+
+        localStorage.setItem(saveds, div.outerHTML);
+
+        inputName.value = "";
+
+        color1.title = "";
+        color1.children[0].setAttribute("style", "display: block");
+        color1.setAttribute("style", "background-image: background-image: url(\"Images/FondoCircle.png\"); border: none; background-color: none");
+        
+        color2.title = "";
+        color2.children[0].setAttribute("style", "display: block");
+        color2.setAttribute("style", "background-image: background-image: url(\"Images/FondoCircle.png\"); border: none; background-color: none");
+
+        color3.title = "";
+        color3.children[0].setAttribute("style", "display: block");
+        color3.setAttribute("style", "background-image: background-image: url(\"Images/FondoCircle.png\"); border: none; background-color: none");
+
+        color4.title = "";
+        color4.children[0].setAttribute("style", "display: block");
+        color4.setAttribute("style", "background-image: background-image: url(\"Images/FondoCircle.png\"); border: none; background-color: none");
+
+        color5.title = "";
+        color5.children[0].setAttribute("style", "display: block");
+        color5.setAttribute("style", "background-image: background-image: url(\"Images/FondoCircle.png\"); border: none; background-color: none");
+    }
 }
